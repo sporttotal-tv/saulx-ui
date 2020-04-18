df28c457c8aa855e351558ae7e2789e034a9f008
youzi <youridaamen@gmail.com>
2020-04-18 10:57:20 +0200

    init switch

--- src/button/Switch.js
+++ src/button/Switch.js
@@ -1,21 +1,24 @@
-import React from "react";
-
-export default () => {
+import React from 'react'
+//<Switch checked={!!elValue} onChange={this.onChange} />
+export default ({ checked, onChange }) => {
   return (
     <div
+      onClick={() => {
+        onChange(!checked)
+      }}
       style={{
         width: 30,
         padding: 5,
-        border: "1px solid black",
+        border: '1px solid black',
       }}
     >
       <div
         style={{
           width: 15,
           height: 15,
-          backgroundColor: "black",
+          backgroundColor: checked ? 'black' : 'lightgrey',
         }}
       ></div>
     </div>
-  );
-};
+  )
+}
