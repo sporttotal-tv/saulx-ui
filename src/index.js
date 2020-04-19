26d043e6e5ae6cf8c4f13fe4aa08fbc352f8b509
youzi <youridaamen@gmail.com>
2020-04-19 17:05:41 +0200

    init options component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import Title from "./text/Title";
-import Header from "./text/Header";
-import Paragraph from "./text/Paragraph";
-import ButtonLight from "./button/ButtonLight";
-import Button from "./button/Button";
-import Text from "./text/Base";
-import Checkbox from "./button/Checkbox";
-import modal from "./overlay/modal";
-import Overlay from "./overlay/Overlay";
-import List from "./List";
-import Tag from "./Tag";
-import Input from "./input/Input";
-import TextArea from "./input/TextArea";
-import Search from "./input/Search";
-import Loader from "./Loader";
-import Switch from "./button/Switch";
+import Title from './text/Title'
+import Header from './text/Header'
+import Paragraph from './text/Paragraph'
+import ButtonLight from './button/ButtonLight'
+import Button from './button/Button'
+import Text from './text/Base'
+import Checkbox from './button/Checkbox'
+import modal from './overlay/modal'
+import Overlay from './overlay/Overlay'
+import List from './List'
+import Tag from './Tag'
+import Input from './input/Input'
+import TextArea from './input/TextArea'
+import Search from './input/Search'
+import Options from './input/Options'
+import Loader from './Loader'
+import Switch from './button/Switch'
 
 export {
   Title,
@@ -32,4 +33,5 @@ export {
   Loader,
   Switch,
   TextArea,
-};
+  Options,
+}
---
+++ src/input/Options.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default ({ style, value, options }) => {
+  return (
+    <select
+      style={{
+        color: 'inherit',
+        appearance: 'none',
+        position: 'relative',
+        backgroundColor: 'transparent',
+        margin: 0,
+        borderRadius: 2.5,
+        border: '1px solid rgba(0,0,0,0.1)',
+        fontFamily:
+          '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        padding: 5,
+        fontSize: 12,
+        ...style,
+      }}
+      value={value || undefined}
+      onChange={this.onChange}
+    >
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  )
+}
