5a89ab5bff56359f3f0d9b279394423fb3de1a7d
Jim de Beer <beerdejim@gmail.com>
2020-04-27 22:19:28 +0200

    fix

--- src/input/Search.js
+++ src/input/Search.js
@@ -18,6 +18,7 @@ const Input = ({ style, onChange, value, placeholder }) => {
           '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
         appearance: 'none',
         fontSize: 12,
+        fontWeight: 'bold',
         ...style
       }}
     />
