57b84485a315d260b7ece6530423dba3e66a7146
Jim de Beer <beerdejim@gmail.com>
2020-04-27 17:10:00 +0200

    remove parent

--- src/Tag.js
+++ src/Tag.js
@@ -1,26 +1,72 @@
 import React from "react";
 import Base from "./text/Base";
+import { useHub } from "@saulx/hub";
 
-export default ({ children, style, onClick }) => {
+const Icon = () => {
+  return (
+    <svg
+      width="8"
+      height="8"
+      viewBox="0 0 24 24"
+      style={{
+        marginRight: 5,
+      }}
+    >
+      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+    </svg>
+  );
+};
+
+export default ({
+  children,
+  style,
+  to,
+  onClose,
+  onClick,
+  color = "black",
+  size = "small",
+}) => {
+  const hub = useHub();
   return (
     <div
       style={{
         width: "auto",
         paddingLeft: 5,
+        display: "flex",
+        alignItems: "center",
         paddingRight: 5,
-        paddingTop: 2.5,
-        paddingBottom: 2.5,
+        fill: color,
+        paddingTop: size === "small" ? 2.5 : size === "large" ? 5 : 0,
+        paddingBottom: size === "small" ? 2.5 : size === "large" ? 5 : 0,
         cursor: "pointer",
-        // borderRadius: 2.5,
         ...style,
       }}
-      onClick={onClick}
+      onClick={to && !onClick ? () => hub.set("device.history", to) : onClick}
     >
+      {onClose ? (
+        <div
+          style={{
+            height: 15,
+            width: 15,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            onClose(e, hub);
+          }}
+        >
+          <Icon />
+        </div>
+      ) : null}
       <Base
         style={{
           alignItems: "center",
           display: "flex",
           fontSize: 10,
+          color,
           fontWeight: "bold",
         }}
       >
