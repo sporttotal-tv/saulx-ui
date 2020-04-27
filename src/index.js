145ae98c716989bc2acd9c7d7e20706b8c5d33f9
youzi <youri@saulx.com>
2020-04-27 22:19:13 +0200

    export icons

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import Title from "./text/Title";
-import Header from "./text/Header";
-import Paragraph from "./text/Paragraph";
-import ButtonLight from "./button/ButtonLight";
-import ButtonSmall from "./button/ButtonSmall";
-import Button from "./button/Button";
-import Text from "./text/Base";
-import Checkbox from "./button/Checkbox";
-import modal from "./overlay/modal";
-import Overlay from "./overlay/Overlay";
-import List from "./List";
-import Tag from "./Tag";
-import Input from "./input/Input";
-import TextArea from "./input/TextArea";
-import Search from "./input/Search";
-import Options from "./input/Options";
-import Loader from "./Loader";
-import Switch from "./button/Switch";
+import Title from './text/Title'
+import Header from './text/Header'
+import Paragraph from './text/Paragraph'
+import ButtonLight from './button/ButtonLight'
+import ButtonSmall from './button/ButtonSmall'
+import Button from './button/Button'
+import Text from './text/Base'
+import Checkbox from './button/Checkbox'
+import modal from './overlay/modal'
+import Overlay from './overlay/Overlay'
+import List from './List'
+import Tag from './Tag'
+import Input from './input/Input'
+import TextArea from './input/TextArea'
+import Search from './input/Search'
+import Options from './input/Options'
+import Loader from './Loader'
+import Switch from './button/Switch'
+// icons
+import SearchIcon from './icon/Search'
+import CloseIcon from './icon/Close'
+import Icon from './icon/Icon'
 
 export {
   Title,
@@ -36,4 +40,8 @@ export {
   Switch,
   TextArea,
   Options,
-};
+  // icons
+  SearchIcon,
+  CloseIcon,
+  Icon
+}
