5172fe6fd6c1978dba9283599a01579382e5df70
Jim de Beer <beerdejim@gmail.com>
2020-05-28 19:10:09 +0200

    fix

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -1,45 +1,47 @@
-import React from "react";
+import React from 'react'
 
-const createSvgCircle = (style) => (
+const createSvgCircle = style => (
   <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4" style={style} />
-);
+)
 
-const Loader = ({ style, size = 20, color = "#000" }) => {
+const Loader = ({ style, size = 20, color = '#000' }) => {
   const svg = (
-    <svg height="100%" viewBox="0 0 32 32" width="100%">
+    <svg viewBox="0 0 32 32" width={size} height={20}>
       {createSvgCircle({
         stroke: color,
-        opacity: 0.2,
+        opacity: 0.2
       })}
       {createSvgCircle({
         stroke: color,
         strokeDasharray: 80,
-        strokeDashoffset: 60,
+        strokeDashoffset: 60
       })}
     </svg>
-  );
+  )
 
   return (
     <div
       style={{
+        maxWidth: size,
+        minWidth: size,
         width: size,
         height: size,
-        animationDuration: "0.75s",
-        animationTimingFunction: "linear",
-        animationIterationCount: "infinite",
-        animationName: "rotate",
-        "@keyframes": {
+        animationDuration: '0.75s',
+        animationTimingFunction: 'linear',
+        animationIterationCount: 'infinite',
+        animationName: 'rotate',
+        '@keyframes': {
           rotate: {
-            "0%": { transform: "rotate(0deg)" },
-            "100%": { transform: "rotate(360deg)" },
-          },
+            '0%': { transform: 'rotate(0deg)' },
+            '100%': { transform: 'rotate(360deg)' }
+          }
         },
-        ...style,
+        ...style
       }}
     >
       {svg}
     </div>
-  );
-};
+  )
+}
 
-export default Loader;
+export default Loader
